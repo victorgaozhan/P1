e44c281a765d11e71968297d9cbf6f1114f6b546
victor.gaozhan <victor.gaozhan@gmail.com>
2023-04-17 21:53:29 +1000

    translate

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
-var tablinks = document.getElementsByClassName("ratelink");
-var tabcontents = document.getElementsByClassName("ratecard");
-var sidemenu = document.getElementById("sidemenu");
-var lang = document.getElementById("language");
+let tablinks = document.getElementsByClassName("ratelink");
+let tabcontents = document.getElementsByClassName("ratecard");
+let sidemenu = document.getElementById("sidemenu");
+let lang = document.getElementById("language");
+let button = document.getElementById("loan_submit");
+let report = document.getElementById("report-contents");
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwTPpuvZKNwGTpKVA44FpJsvBicwyugb1V79_cCkeG4Np4JxmWPHQW8qclrYU0EUeHygQ/exec'
 const form = document.forms['submit-to-google-sheet']
@@ -71,7 +73,7 @@ const contents = [
   new Translation("title", "tag", ["Mortgage Broker: Sally"], null),
   new Translation("sidemenu", "id", null, 
   [
-    { tagName: "a", values: ["Home", "About", "Rates", "Service", "Application", "Contact","AI Evaluation"]},
+    { tagName: "a", values: ["Home", "About", "Rates", "Service", "Application", "Contact","A.I. Evaluation<span class='nav-new'>New!</span>"]},
   ]),
   new Translation("header", "id", null, 
   [
@@ -79,7 +81,7 @@ const contents = [
     { tagName: "h1", values: ["I am <span>Sally. </span><br>Your Mortgage Broker"]},
   ]),    
   new Translation("sub-title", "class", 
-  ["Why do you trust me?", "Interest Rates", "Five Star Financial Service", "Application", "Contacts", "Chat-GPT"], null),   
+  ["Why do you trust me?", "Interest Rates", "Five Star Financial Service", "Application", "Contacts", "A.I. Evaluation"], null),   
   new Translation("about-list", "class", null, 
   [
     { tagName: "h2", values: ["10+ years", "100+ Million", "1000+", "50+","Qulification","Education"]},
@@ -125,6 +127,7 @@ const contents = [
     {tagName:"div", values:["Single","Joint Income","Owner occupied","Investment"]}
   ]),
   new Translation("button","tag",["Submit", "Submit"], null),
+  new Translation("report-title","class",["Reported by Chat-GPT"], null),
 ];
 
 function change_lang(){
@@ -140,11 +143,11 @@ function change_lang(){
 }
 
 function init_lang(){
-  var lang = "";
+  let lang = "";
   const queryString = window.location.search;
   if(queryString.length > 0)
   {
-    var urlParams = new URLSearchParams(queryString);
+    let urlParams = new URLSearchParams(queryString);
     lang = urlParams.get('lang');
   }
 
@@ -186,7 +189,7 @@ function init_lang(){
 
           if(element.length > 0){
               for (let i = 0; i < element.length; i++) {
-                var tags = element[i].getElementsByTagName(tag.tagName);
+                let tags = element[i].getElementsByTagName(tag.tagName);
                 for (let j = 0; j < tags.length; j++) {
                   tagNames.push(tags[j]);
                 }
@@ -207,14 +210,70 @@ function init_lang(){
 function sentApplication(){
   
   const uri = "https://localhost:7246/api/ChatGPT";
-  const salary1Textbox = document.getElementById("salary1");
+  const salary1 = document.getElementById("salary1").value.trim();
 
+  const salary1Period = document.getElementById("salary1_period").selectedOptions[0].value;
+  const salary2 = document.getElementById("salary2").value.trim();
+  const salary2Period = document.getElementById("salary2_period").selectedOptions[0].value;
+  const other1 = document.getElementById("other1").value.trim();
+  const other1Period = document.getElementById("other1_period").selectedOptions[0].value;
+  const other2 = document.getElementById("other2").value.trim();
+  const other2Period = document.getElementById("other2_period").selectedOptions[0].value;
+  const other3 = document.getElementById("other3").value.trim();
+  const other3Period = document.getElementById("other3_period").selectedOptions[0].value;
+
+  const expenses = document.getElementById("expenses").value.trim();
+  const expensesPeriod = document.getElementById("expenses_period").selectedOptions[0].value;
+  const rent = document.getElementById("rent").value.trim();
+  const rentPeriod = document.getElementById("rent_period").selectedOptions[0].value;
+  const carloans = document.getElementById("carloans").value.trim();
+  const carloansPeriod = document.getElementById("carloans_period").selectedOptions[0].value;
+  const loans = document.getElementById("loans").value.trim();
+  const loansPeriod = document.getElementById("loans_period").selectedOptions[0].value;
+  const loans_outstanding = document.getElementById("loans_outstanding").value.trim();
+  const loans_term = document.getElementById("loans_term").value.trim();
+  const card_limit = document.getElementById("card_limit").value.trim();
+  const children = document.getElementById("children").selectedOptions[0].value;
+  const term = document.getElementById("term").value.trim();
+
+  const incomeType = document.getElementsByClassName("incomelink active-link")[0].id;
+  const loanType = document.getElementsByClassName("loanlink active-link")[0].id;
+  
   const item = {
-    salary1: salary1Textbox.value.trim(),
-    age: 0,
-    marriagestatus: "string",
-    purpose: "string",
+    language: lang.selectedIndex,
+    salary1: ConvertToAnnually(salary1, salary1Period),
+    salary2: ConvertToAnnually(salary2, salary2Period),
+    otherIncome: ConvertToAnnually(other1, other1Period) 
+      + ConvertToAnnually(other2, other2Period) 
+      + ConvertToAnnually(other3, other3Period),
+
+    expenses: ConvertToAnnually(expenses, expensesPeriod)
+      + ConvertToAnnually(rent, rentPeriod)
+      + ConvertToAnnually(carloans, carloansPeriod),
+
+    loans: ConvertToAnnually(loans, loansPeriod),
+    loans_outstanding: loans_outstanding,
+    loans_term: loans_term,
+    card_limit: card_limit,
+    children: children,
+    term: term,
+    incomeType: incomeType,
+    loanrate: (loanType == "loan_type1" ? "5.38" : "5.58"),
+    IsInvestment: (loanType == "loan_type1" ? false : true),
   }
+
+  let results = document.getElementById("chatGPT-report");
+  results.style.display = "grid";
+
+
+  report.style.height = "auto";
+  report.innerText = item.language == 0 ? "Thank you for your submit, Chat-GPT is connecting. Please wait about 1 minute": "感谢您的提交, Chat-GPT连接中, 约1分钟左右。请稍候";
+  report.classList.add("loading");
+
+  let reportNotice = document.getElementById("report-notice");
+  reportNotice.innerText = "";
+
+  button.disabled = true;
 
   fetch(uri, {
     method: 'POST',
@@ -226,11 +285,45 @@ function sentApplication(){
   })
   .then(response => response.json())
   .then(data => {
-    handle(data);
+    handle(data, true);
   })
-  .catch(error=> console.error('', error));
+  .catch(error=> handle(error, false));
+
 }
 
-function handle(data){
+function handle(data, success){
+  //let report = document.getElementById("report-contents");
+  report.style.height = "auto";
 
+  let reportNotice = document.getElementById("report-notice");
+  if(success){
+    report.innerText = data;
+    reportNotice.innerText = lang.selectedIndex == 0 ? "The content of the above assessment report comes from Chat-GPT, and the content of the assessment is for reference only. If you want to get a more accurate assessment of loan capacity, please contact Sally Gong, thank you.": "以上评估报告内容来自Chat-GPT, 评估内容仅供参考, 不能作为贷款依据。想要获得更准确的贷款能力评估, 敬请联系Sally Gong, 谢谢。";
+  }
+  else{
+    report.innerText = (lang.selectedIndex == 0 ? "Sorry, System Error. Please try it later. " : "抱歉，系统提交失败，请稍候再试。 ") + data;
+  }
+
+  button.disabled = false;
+  report.classList.remove("loading");
+ 
+}
+
+function ConvertToAnnually(amount, period){
+  switch (period)
+  {
+      case "Monthly":
+      case "/月":
+          return amount * 12;
+      case "Weekly":
+      case "/周":
+          return amount * 52;
+      case "Fortnightly":
+      case "/双周":
+          return amount * 26;
+      case "Annually":
+      case "/年":
+      default:
+          return Number(amount);
+  }
 }
